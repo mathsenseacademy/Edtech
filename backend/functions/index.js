a8829187834022efd73e3ebd5b2b1200528873bb
sayank22 <sayank10023@gmail.com>
2025-12-01 17:59:37 +0530

    change backend/functions/index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,4 @@
-// backend/functions/index.js
-
-import functions from "firebase-functions";
+import * as functions from "firebase-functions";
 import app from "./server.js";
 
 export const api = functions.https.onRequest(app);
